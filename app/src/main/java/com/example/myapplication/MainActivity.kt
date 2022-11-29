01253dd03a8c4cd55e3c4496281ee23a7f0d38fb
yana-saav <yana.work88@gmail.com>
2022-11-29 17:27:05 +0200

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,80 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.KeyEvent
+import android.view.View
+import com.example.myapplication.databinding.ActivityMainBinding
+import net.objecthunter.exp4j.ExpressionBuilder
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            binding = ActivityMainBinding.inflate(layoutInflater)
+            val view = binding.root
+            setContentView(view)
+
+            binding.btnPlus.setOnClickListener{setTextFields("+")}
+            binding.btnMinus.setOnClickListener{setTextFields("-")}
+            binding.btnMulti.setOnClickListener{setTextFields("*")}
+            binding.btnDiv.setOnClickListener{setTextFields("/")}
+
+            binding.mathNumbers.setOnKeyListener(object : View.OnKeyListener {
+                override fun onKey(v: View?, keyCode: Int, event: KeyEvent): Boolean {
+                    if (event.action == KeyEvent.ACTION_DOWN &&
+                        keyCode == KeyEvent.KEYCODE_ENTER
+                    ) {
+                        setTextFields1()
+                        binding.mathNumbers.isCursorVisible = false
+                        return true
+                    }
+                    return false
+                }
+            })
+
+
+            binding.btnBack.setOnClickListener {
+                val str = binding.mathOperation.text.toString()
+                if(str.isNotEmpty()){
+                    binding.mathOperation.text = str.substring(0,str.length-1)
+                }
+            }
+
+
+            binding.btnEquals.setOnClickListener{
+                try {
+                    val ex = ExpressionBuilder(binding.mathOperation.text.toString()).build()
+                    val result = ex.evaluate()
+                    val intent = Intent(this, TwoActivity::class.java)
+                    intent.putExtra("result", result)
+                    startActivity(intent)
+                }catch (e:Exception) {
+                    Log.d("Error","message: ${e.message}")
+                }
+
+
+        }
+    }
+
+    fun setTextFields(str: String){
+
+        if(binding.mathNumbers.text.isNotEmpty()) {
+            binding.mathOperation.append(binding.mathNumbers.text)
+            binding.mathNumbers.setText("")
+        }
+
+        binding.mathOperation.append(str)
+    }
+
+
+    fun setTextFields1(){
+
+        if( binding.mathNumbers.text.isNotEmpty()) {
+            binding.mathOperation.append(binding.mathNumbers.text)
+            binding.mathNumbers.setText("")
+        }
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/TwoActivity.kt
@@ -0,0 +1,19 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class TwoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_two)
+
+        val result =findViewById<TextView>(R.id.result)
+        val result1= intent.getDoubleExtra("result",0.0)
+        val longRes = result1.toLong()
+        if (result1 == longRes.toDouble())
+            result.text=longRes.toString()
+        else  result.text=result1.toString()
+    }
+}
